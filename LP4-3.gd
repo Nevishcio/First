e393bfffa2a36b12e4e75739652ce80d58c714f7
Nevishcio <eviliceisinsane@gmail.com>
2023-09-19 12:47:15 -0500

    Squegg

--- LP4-3.gd
+++ LP4-3.gd
@@ -12,4 +12,22 @@ func _process(delta):
 
 
 func _on_calculate_pressed():
-	pass # Replace with function body.
+	var copies = int($TextCopies.text)
+	var price = 0.0
+	var cost = 0.0
+	if copies > 0 and copies <= 99:
+		price = 0.30
+	elif copies > 99 and copies <= 499:
+		price = 0.28
+	elif copies > 499 and copies <= 749:
+		price = 0.27
+	elif copies > 749 and copies <= 1000:
+		price = 0.26
+	elif copies > 1000:
+		price = 0.25
+	else:
+		$LabelOut.text = "Invalid Number of Egg Dozens"
+		return
+	cost = price * copies
+	$LabelOut.text = "Price per Egg Dozen is $" + str(price) + "\nTotal cost is $%.2f" % cost
+	pass
