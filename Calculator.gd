454c0edde26921075720c2c40ea726bd2b890551
Nevishcio <eviliceisinsane@gmail.com>
2023-10-05 11:09:15 -0500

    Free

--- Calculator.gd
+++ Calculator.gd
@@ -15,35 +15,69 @@ var num2 = 0
 func getlineedit(): 
 	num1 = int($LineEdit1.text)
 	num2 = int($LineEdit2.text)
-func add(): 
-	(num1 + num2)
-func sub(): (num1 - num2)
+
+func add():
+	var add = num1 + num2
+	$LabelOutput.text = "" + str(add)
+
+func sub():
+	var sub = num1 - num2
+	$LabelOutput.text = "" + str(sub)
+
 func multiply():
-	$LabelOutput.text = ""
-	(num1 * num2)
-func divide(): (num1 / num2)
+	var multiply = num1 * num2
+	$LabelOutput.text = "" + str(multiply)
+
+func divide():
+	var divide = num1 / num2
+	$LabelOutput.text = "" + str(divide)
+
+func POW():
+	var power = num1 ** num2
+	$LabelOutput.text = "" + str(power)
+
+func mod():
+	var mod = num1 % num2
+	$LabelOutput.text = "" + str(mod)
+
 
 
 
 func _on_clear_pressed():
-	pass # Replace with function body.
+	$LabelOutput.text = ""
+	$LineEdit1.text = ""
+	$LineEdit2.text = ""
 
 
 func _on_quit_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
 
 
 func _on_multiply_pressed():
+	getlineedit()
 	multiply()
 
 
 func _on_divide_pressed():
+	getlineedit()
 	divide()
 
 
 func _on_add_pressed():
+	getlineedit()
 	add()
 
 
 func _on_subtract_pressed():
+	getlineedit()
 	sub()
+
+
+func _on_pow_pressed():
+	getlineedit()
+	POW()
+
+
+func _on_mod_pressed():
+	getlinjeedit()
+	mod()
