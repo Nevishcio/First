3ad8fb3e227dc1dc50003fd966679d6c0be0f646
Nevishcio <eviliceisinsane@gmail.com>
2023-10-11 13:10:05 -0500

    I didn't get to do anything ;(

---
+++ Enemy.gd
@@ -0,0 +1,20 @@
+extends RigidBody2D
+
+var speed = 100
+var direction = -1
+
+func _ready():
+	$Timer.start()
+
+func _physics_process(_delta):
+	self.linear_velocity.x = speed * direction
+
+func _on_timer_timeout():
+	direction *= -1
+
+
+
+func _on_body_entered(body):
+	if body.name == "GodotGuy":
+		body.que_free()
+		OS.alert("You are dead")
--- keyboard_control.gd
+++ keyboard_control.gd
@@ -9,14 +9,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if dragging:
-		$Draggable.global_position = get_global_position() - offset
+		$Draggable.global_position = get_global_mouse_position() - offset
 
 var dragging = false
 var offset = Vector2.ZERO
 
 func _on_interact_mouse_entered():
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		$interact.queue_free()
+		$Interact.queue_free()
 
 
 func _on_button_button_down():
