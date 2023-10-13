d1f32a42dea1eb13714b40d4de84d804c120c768
Nevishcio <eviliceisinsane@gmail.com>
2023-10-13 12:52:04 -0500

    NewIdea

--- GodotGuy.gd
+++ GodotGuy.gd
@@ -2,16 +2,16 @@ extends CharacterBody2D
 
 
 const SPEED = 600.0
-const JUMP_VELOCITY = -700.0
+const JUMP_VELOCITY = 000.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * _delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
