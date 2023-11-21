ee0deaf1a429eae83c49a9e93c4e0fdac72864c6
Nevishcio <eviliceisinsane@gmail.com>
2023-11-21 12:48:45 -0600

    SAVE THIS TIME

--- ADV3NTUR3H3R0.gd
+++ ADV3NTUR3H3R0.gd
@@ -1,19 +1,41 @@
 extends CharacterBody2D
 
+enum STATES {IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING}
+
 @export var data = {
 	"max_health": 60.0,
 	"health": 60.0,
 	"money": 0,
 	"book": 0,
+	"state": STATES.IDLE,
+	"secondaries": [],
 }
 const SPEED = 100
 const MAX_OBTAINABLE_HEALTH = 400.0
 var inertia = Vector2()
 var look_direction = Vector2.DOWN
+var attack_direction = Vector2.DOWN
+var animation_lock = 0.0
+var slash_scene = ("res://entities/attacks/slash.tscn")
 var menu_scene = preload("res://gui.tscn")
 var menu_instance = null
 
 @onready var HUD = get_tree().get_first_node_in_group("HUD")
+
+func get_direction_name():
+	return ["right", "down", "left", "up"][
+		int(round(look_direction.angle() * 2 / PI)) % 4
+	]
+
+func attack():
+	data.state = STATES.ATTACKING
+	$AnimatedSprite2D.play("attack_" + get_direction_name())
+	attack_direction = look_direction
+	var slash = slash_scene.instantiate()
+	slash.position = attack_direction * 20
+	slash.rotation = Vector2().angle_to_point(-attack_direction)
+	add_child(slash)
+	animation_lock = 0.2
 
 func pickup_money(value):
 	data.money += value
@@ -45,6 +67,10 @@ func _physics_process(_delta):
 	velocity += inertia
 	move_and_slide()
 	inertia = inertia.move_toward(Vector2(),_delta * 1000.0)
+	
+	if data.state != STATES.DEAD:
+		if Input.is_action_just_pressed("ui_accept"):
+			attack()
 
 
 func update_animation(direction):
