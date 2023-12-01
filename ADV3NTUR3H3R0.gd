80cacc57c5ea0dc42fa56c23d0f42a63bdad45d9
Nevishcio <eviliceisinsane@gmail.com>
2023-12-01 12:53:08 -0600

    Totally my code

--- ADV3NTUR3H3R0.gd
+++ ADV3NTUR3H3R0.gd
@@ -1,42 +1,47 @@
 extends CharacterBody2D
 
-enum STATES {IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING}
+
+const SPEED = 100.0  # 80.0
+# const JUMP_VELOCITY = -400.0
+const MAX_OBTAINABLE_HEALTH = 400.0
+
+enum STATES { IDLE=0, DEAD, DAMAGED, ATTACKING, CHARGING }
 
 @export var data = {
-	"max_health": 60.0,
-	"health": 60.0,
+	"max_health": 60.0,  # 20hp per heart; 5 per fraction
+	"health": 60.0,      # Min 60 Max 400
 	"money": 0,
-	"book": 0,
 	"state": STATES.IDLE,
 	"secondaries": [],
 }
-const SPEED = 100
-const MAX_OBTAINABLE_HEALTH = 400.0
+
 var inertia = Vector2()
-var look_direction = Vector2.DOWN
+var look_direction = Vector2.DOWN  # (0, 1)
 var attack_direction = Vector2.DOWN
-var animation_lock = 0.0
+var animation_lock = 0.0  # Lock player while playing attack animation
 var damage_lock = 0.0
 var charge_time = 2.5
 var charge_start_time = 0.0
 
 var slash_scene = preload("res://entities/attacks/slash.tscn")
-var menu_scene = preload("res://gui.tscn")
+var menu_scene = preload("res://HUD.tscn")
 var menu_instance = null
 
-@onready var HUD = get_tree().get_first_node_in_group("HUD")
+@onready var p_HUD = get_tree().get_first_node_in_group("HUD")
 
 func get_direction_name():
-	return ["side", "down", "side", "up"][
+	return ["right", "down", "left", "up"][
 		int(round(look_direction.angle() * 2 / PI)) % 4
 	]
 
 func attack():
 	data.state = STATES.ATTACKING
+	if get_direction_name() == "left":
+		$AnimatedSprite2D.flip_h = 0
 	$AnimatedSprite2D.play("attack_" + get_direction_name())
 	attack_direction = look_direction
 	var slash = slash_scene.instantiate()
-	slash.position = attack_direction * 20
+	slash.position = attack_direction * 20.0
 	slash.rotation = Vector2().angle_to_point(-attack_direction)
 	add_child(slash)
 	animation_lock = 0.2
@@ -48,82 +53,101 @@ func charged_attack():
 	damage_lock = 0.3
 	
 	for i in range(9):
-		var angle = -attack_direction.angle() + (i-4) * PI / 4;
-		var direction = Vector2(cos(angle), sin(angle))
+		# Offset by (i-4) * 45 degrees
+		var angle = -attack_direction.angle() + (i-4) * PI / 4;  # [-4,4]
+		var dir = Vector2(cos(angle), sin(angle))
 		var slash = slash_scene.instantiate()
-		slash.position = direction * 20.0
-		slash.rotation = Vector2().angle_to_point(-direction)
+		slash.position = dir * 20.0
+		slash.rotation = Vector2().angle_to_point(-dir)
 		slash.damage *= 1.5
 		add_child(slash)
 		await get_tree().create_timer(0.03).timeout
+	
 	animation_lock = 0.2
-	data.state = STATES.IDLE
 	await $AnimatedSprite2D.animation_finished
+	data.state = STATES.IDLE
 	pass
 
 func pickup_money(value):
 	data.money += value
 
-func pickup_heart(value):
+func pickup_health(value):
 	data.health += value
 	data.health = clamp(data.health, 0, data.max_health)
 
-func pickup_book(value):
-	data.book += value
+func _ready():
+	p_HUD.show()
+	menu_instance = menu_scene.instantiate()
+	get_tree().get_root().add_child.call_deferred(menu_instance)
+	menu_instance.hide()
 
-func take_damaage(dmg):
+
+signal health_depleted
+
+func take_damage(dmg):
+	if damage_lock == 0.0:
+		data.health -= dmg
+		data.state = STATES.DAMAGED
+		damage_lock = 0.5
+		animation_lock = dmg * 0.005
+		# TODO: damage shader
+		if data.health <= 0:
+			data.state = STATES.DEAD
+			# TODO: play death animation & sound
+			await get_tree().create_timer(0.5).timeout
+			health_depleted.emit()
+		else:
+			# TODO: play damage sound
+			pass
 	pass
 
-func _ready():
-	HUD.show()
-	menu_instance = menu_scene.instantiate()
-	add_child(menu_instance)
-	menu_instance.hide()
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().get_root().add_child.call_deferred(menu_instance)
-		get_tree().paused = true
 
-func _physics_process(_delta):
-	animation_lock = max(animation_lock-_delta, 0.0)
-	if data.state != STATES.CHARGING:
-			data.state = STATES.IDLE
-	damage_lock = max(damage_lock-_delta, 0.0)
+func _physics_process(delta):
+	animation_lock = max(animation_lock-delta, 0.0)
+	damage_lock = max(damage_lock-delta, 0.0)
+	
 	if animation_lock == 0.0 and data.state != STATES.DEAD:
+		# TODO: damage and charging
+		if data.state != STATES.CHARGING:
+			data.state = STATES.IDLE
+		
 		var direction = Vector2(
 			Input.get_axis("ui_left", "ui_right"),
-			 Input.get_axis("ui_up", "ui_down")
-			).normalized()
+			Input.get_axis("ui_up", "ui_down")
+		).normalized()  # Scale to 1 to prevent speed boost
 		update_animation(direction)
 		if direction.length() > 0:
 			look_direction = direction
 			velocity = direction * SPEED
-	else:
-		velocity = velocity.move_toward(Vector2(), SPEED)
-	velocity += inertia
-	move_and_slide()
-	inertia = inertia.move_toward(Vector2(),_delta * 1000.0)
+		else:
+			velocity = velocity.move_toward(Vector2(), SPEED)
+		
+		velocity += inertia
+		move_and_slide()
+		inertia = inertia.move_toward(Vector2(), delta * 1000.0)
 	
 	if data.state != STATES.DEAD:
 		if Input.is_action_just_pressed("ui_accept"):
 			attack()
 			charge_start_time = Time.get_time_dict_from_system().second
 			data.state = STATES.CHARGING
-	if Input.is_action_just_released("ui_accept"):
-		var charge_duration = Time.get_time_dict_from_system().second - charge_start_time
-		if charge_duration >= charge_time and data.state == STATES.CHARGING:
-			charged_attack()
-		else:
-			data.state = STATES.IDLE
+		
+		if Input.is_action_just_released("ui_accept"):
+			var ctime = Time.get_time_dict_from_system().second
+			var charge_duration = ctime - charge_start_time
+			if charge_duration >= charge_time and data.state == STATES.CHARGING:
+				charged_attack()
+			else:
+				data.state = STATES.IDLE
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		menu_instance.show()
 		get_tree().paused = true
 
 
-
-
 func update_animation(direction):
 	if data.state == STATES.IDLE:
-		var a_name = "idle_down"
+		var a_name = "idle_down"  # Default
 		if direction.length() > 0:
 			look_direction = direction
 			a_name = "walk_"
--- TreeEnemy.gd
+++ TreeEnemy.gd
@@ -1,30 +1,27 @@
 extends CharacterBody2D
 
 
-const SPEED = 75.0
+const SPEED = 60.0
 var MAX_HEALTH: float = 30.0
 var HEALTH = MAX_HEALTH
 var DAMAGE = 10.0
 var AI_STATE = STATES.IDLE
-enum STATES {IDLE = 0, UP, DOWN, LEFT, RIGHT,
-			UP_L, UP_R, DOWN_L, DOWN_R, DAMAGED}
+
+enum STATES { IDLE=0, UP, DOWN, LEFT, RIGHT,
+			  UP_L, UP_R, DOWN_L, DOWN_R, DAMAGED }
+
 var state_directions = [
 	Vector2.ZERO,
 	Vector2.UP,
 	Vector2.DOWN,
 	Vector2.LEFT,
 	Vector2.RIGHT,
-	Vector2(-1, -1).normalized(),
-	Vector2(1, -1).normalized(),
-	Vector2(-1, 1).normalized(),
-	Vector2(1, 1).normalized(),
+	Vector2(-1, -1).normalized(),  # UL
+	Vector2(1, -1).normalized(),   # UR
+	Vector2(-1, 1).normalized(),   # DL
+	Vector2(1, 1).normalized(),    # DR
 	Vector2.ZERO
 ]
-
-signal recovered
-@onready var raycast1 = $RayCast1
-@onready var raycast2 = $RayCast2
-@onready var raycast3 = $RayCast3
 
 var inertia = Vector2()
 var ai_timer_max = 0.5
@@ -35,56 +32,92 @@ var knockback = 128.0
 var vision_distance = 40.0
 var money_value = 5.0
 
+signal recovered
+
+@onready var raycastR = $RayCast2DR
+@onready var raycastM = $RayCast2DM
+@onready var raycastL = $RayCast2DL
+
 func turn_toward_player_location(location: Vector2):
-	var direction_to_player = (location - global_position).normalized()
-	velocity = direction_to_player * (SPEED * 2)
+	# Set the satte to move toward the player
+	var dir_to_player = (location - global_position).normalized()
+	velocity = dir_to_player * (SPEED * 2)
+	# Determine the closest cardinal direction for animation
 	var closest_angle = INF
 	var closest_state = STATES.IDLE
 	for i in range(1, 5):
-		var state_direction = state_directions[i]
-		var angle_diff = abs(state_direction.angle_to(direction_to_player))
+		var state_dir = state_directions[i]
+		var angle_diff = abs(state_dir.angle_to(dir_to_player))
 		if angle_diff < closest_angle:
 			closest_angle = angle_diff
 			closest_state = STATES.values()[i]
 	AI_STATE = closest_state
 
-func take_damaage(dmg, attacker = null):
+
+func take_damage(dmg, attacker=null):
+	if damage_lock == 0.0:
+		AI_STATE = STATES.DAMAGED
+		HEALTH -= dmg
+		damage_lock = 0.2
+		animation_lock = 0.2
+		# TODO: damage intensity & shader
+		if HEALTH <= 0:
+			# TODO: drop item and play sound
+			queue_free()
+		else:
+			if attacker != null:
+				var location = attacker.global_position
+				await recovered
+				turn_toward_player_location(location)
 	pass
 
-func _physics_process(_delta):
-	animation_lock = max(animation_lock-_delta, 0.0)
-	damage_lock = max(damage_lock-_delta, 0.0)
-	if int(AI_STATE) >= 1 and int(AI_STATE)<= 8:
+
+func _physics_process(delta):
+	animation_lock = max(animation_lock-delta, 0.0)
+	damage_lock = max(damage_lock-delta, 0.0)
+	if int(AI_STATE) >= 1 and int(AI_STATE) <= 8:
 		var raydir = state_directions[int(AI_STATE)]
-		raycast1.target_position = raydir * vision_distance
-		raycast2.target_position = raydir.rotated(deg_to_rad(-45)).normalized * vision_distance
-		raycast3.target_position = raydir.rotated(deg_to_rad(45)).normalized * vision_distance
-		if animation_lock == 0.0:
-			if AI_STATE == STATES.DAMAGED:
+		raycastM.target_position = raydir * vision_distance
+		raycastL.target_position = \
+			raydir.rotated(deg_to_rad(-45)).normalized() * vision_distance
+		raycastR.target_position = \
+			raydir.rotated(deg_to_rad(45)).normalized() * vision_distance
+	
+	if animation_lock == 0.0:
+		if AI_STATE == STATES.DAMAGED:
+			AI_STATE = STATES.IDLE
+			recovered.emit()
+		
+		for player in get_tree().get_nodes_in_group("Player"):
+			if $AttackBox.overlaps_body(player):
+				if player.damage_lock == 0.0:
+					player.inertia = \
+						(player.global_position-global_position).normalized() \
+							* knockback
+					player.take_damage(DAMAGE)
+				else:
+					return
+			if player.data.state != player.STATES.DEAD:
+				if (raycastM.is_colliding() and raycastM.get_collider() == player) or \
+				   (raycastL.is_colliding() and raycastL.get_collider() == player) or \
+				   (raycastR.is_colliding() and raycastR.get_collider() == player):
+					turn_toward_player_location(player.global_position)
+		
+		ai_timer = clamp(ai_timer-delta, 0.0, ai_timer_max)
+		if ai_timer == 0.0:
+			if AI_STATE == STATES.IDLE:
+				var rnd_mov = randi() % 4
+				AI_STATE = STATES.values()[rnd_mov+1]
+			else:
 				AI_STATE = STATES.IDLE
-				recovered.emit()
-			for player in get_tree().get_nodes_in_group("Player"):
-				if $AttackBox.overlaps_body(player):
-					if player.gamaged_lock == 0.0:
-						player.inertia = (player.global_position-global_position).normalized() * knockback
-						player.take_damage(DAMAGE)
-					else:
-						return
-				if player.data.state != player.STATES.DEAD:
-					if (raycast1.is_colliding() and raycast1.gel_collider() == player) or \
-					   (raycast1.is_colliding() and raycast1.gel_collider() == player) or \
-					   (raycast1.is_colliding() and raycast1.gel_collider() == player):
-						turn_toward_player_location(player.global_position)
-	ai_timer = clamp(ai_timer - _delta, 0.0, ai_timer_max)
-	if ai_timer == 0.0:
-		if AI_STATE == STATES.IDLE:
-			var _random_move = randi() % 4
-			AI_STATE == STATES.values()[_random_move+1]
-		else:
-			AI_STATE == STATES.IDLE
-		ai_timer = ai_timer_max
-	var direction = state_directions[int(AI_STATE)]
-	velocity = direction * SPEED
-	velocity += inertia
-	move_and_slide()
-	inertia = inertia.move_toward(Vector2(), _delta * 1000)
+			ai_timer = ai_timer_max
+		
+		var direction = state_directions[int(AI_STATE)]
+		velocity = direction * SPEED
+		
+		# TODO: walk animation stuff
+		
+		velocity += inertia
+		move_and_slide()
+		inertia = inertia.move_toward(Vector2(), delta * 1000.0)
+	pass
