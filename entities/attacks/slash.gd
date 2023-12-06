653af5aad5c570c1ffa1a222fd77aebcc2e87d8a
Nevishcio <eviliceisinsane@gmail.com>
2023-12-06 13:02:38 -0600

    NGGYU

--- entities/attacks/slash.gd
+++ entities/attacks/slash.gd
@@ -1,16 +1,18 @@
 extends Area2D
 
 var damage = 10
-var knockback = 64.2
+var knockback = 128.0
 var anim_life = 0.2
 
-func _process(_delta):
-	anim_life -= _delta
+func _process(delta):
+	anim_life -= delta
 	if anim_life < 0:
 		queue_free()
 		return
+	
 	for enemy in get_tree().get_nodes_in_group("Enemy"):
 		if overlaps_body(enemy):
 			enemy.take_damage(damage, self)
 			enemy.inertia = (enemy.global_position-self.global_position) \
-						.normalized() * knockback
+							.normalized() * knockback
+	pass
